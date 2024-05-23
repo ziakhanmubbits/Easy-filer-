785b61d0cb8a49b078fbfcbb20df66dea6362d84
ziakhanmubbits <136321080+ziakhanmubbits@users.noreply.github.com>
2024-05-23 13:00:33 +0500

    initiate
    initiate

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+   <View>
+    <Text>d;d;</Text>
+   </View>
+  )
+}       
+
+export default   App
